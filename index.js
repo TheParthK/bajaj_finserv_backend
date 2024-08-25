d02121526e18928ddd8d5993a4fbb795647d6046
Parth Kalia <95091403+TheParthK@users.noreply.github.com>
2024-08-25 11:43:04 +0530

    added cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors()); 
 const PORT = process.env.PORT || 3000;
 
 
---
+++ script.js
@@ -0,0 +1,25 @@
+function sendData() {
+    const dataInput = document.getElementById('data').value;
+    const dataArray = dataInput.split(',').map(item => item.trim());
+
+    const payload = {
+        data: dataArray
+    };
+
+    fetch('https://bajaj-finserv-backend-wn08.onrender.com/bfhl', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const outputDiv = document.getElementById('output');
+        outputDiv.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('output').innerText = 'An error occurred. Please check the console for details.';
+    });
+}
